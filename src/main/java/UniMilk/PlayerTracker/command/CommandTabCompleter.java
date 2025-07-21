7d65b2ee73ffcc1efe7dcf6895e3db8965769543
UniMilk <dujinhan2006@outlook.com>
2025-07-21 12:48:00 +0800

    fix bug: players without specific permission can see the commands

--- src/main/java/UniMilk/PlayerTracker/command/CommandTabCompleter.java
+++ src/main/java/UniMilk/PlayerTracker/command/CommandTabCompleter.java
@@ -17,29 +17,43 @@ public class CommandTabCompleter implements TabCompleter {
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         // 补全 `/playertracker ?` 或 `/pt ?`
         if (args.length == 1) {
-            return getListStartsWith(Arrays.asList("track", "viewer", "log", "reload", "help"), args[0]);
+            List<String> list = new ArrayList<>();
+            if (sender.hasPermission("playertracker.use")) {
+                // 如果玩家有 "playertracker.use" 权限，添加 track 命令
+                list.add("track");
+            }
+            if (sender.hasPermission("playertracker.admin")) {
+                // 如果玩家有 "playertracker.admin" 权限，添加 log 和 reload 命令
+                list.add("log");
+                list.add("reload");
+            }
+            if (sender.hasPermission("playertracker.view")) {
+                // 如果玩家有 "playertracker.view" 权限，添加 viewer 命令
+                list.add("viewer");
+            }
+            return getListStartsWith(list, args[0]);
         }
 
         // 补全 `/playertracker track ?` 或 `/pt track ?`
-        if (args.length == 2 && args[0].equalsIgnoreCase("track")) {
+        if (args.length == 2 && args[0].equalsIgnoreCase("track") && sender.hasPermission("playertracker.use")) {
             return getPlayerNameList(args[1]);
         }
 
         // 补全 `/playertracker viewer ?` 或 `/pt viewer ?`
-        if (args.length == 2 && args[0].equalsIgnoreCase("viewer")) {
+        if (args.length == 2 && args[0].equalsIgnoreCase("viewer") && sender.hasPermission("playertracker.view")) {
             return getListStartsWith(Arrays.asList("add", "remove"), args[1]);
         }
         // 补全 `/pt viewer add/remove ?`
-        if (args.length == 3 && args[0].equalsIgnoreCase("viewer")) {
+        if (args.length == 3 && args[0].equalsIgnoreCase("viewer") && sender.hasPermission("playertracker.view")) {
             return getPlayerNameList(args[2]);
         }
 
         // 补全 `/playertracker log ?` 或 `/pt log ?`
-        if (args.length == 2 && args[0].equalsIgnoreCase("log")) {
+        if (args.length == 2 && args[0].equalsIgnoreCase("log") && sender.hasPermission("playertracker.admin")) {
             return getListStartsWith(Arrays.asList("on", "off", "schedule"), args[1]);
         }
         // 补全 `/pt log schedule`
-        if (args.length == 3 && args[0].equalsIgnoreCase("log")) {
+        if (args.length == 3 && args[0].equalsIgnoreCase("log") && sender.hasPermission("playertracker.admin")) {
             return Arrays.asList("<int>");
         }
 
