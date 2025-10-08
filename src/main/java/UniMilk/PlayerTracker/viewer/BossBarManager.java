e9bec64c98155bfb6359df066be6083dd6b60123
UniMilk <dujinhan2006@outlook.com>
2025-10-08 22:12:45 +0800

    start switching to adventure API in BossBarManager.java

--- src/main/java/UniMilk/PlayerTracker/viewer/BossBarManager.java
+++ src/main/java/UniMilk/PlayerTracker/viewer/BossBarManager.java
@@ -4,10 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
-import org.bukkit.boss.*;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.entity.Boss;
 import org.bukkit.entity.Player;
 
-import net.md_5.bungee.api.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.bossbar.BossBar.Color;
 
 import unimilk.playertracker.util.DirectionDistanceCalc;
 
@@ -15,9 +21,9 @@ public class BossBarManager {
     // BossBar管理器类
     private final Map<Player, BossBar> bars = new HashMap<>(); // 定义玩家和BossBar的映射Map
 
-    public void addBossBar(Player player, String title) {
+    public void addBossBar(Player player, Component title) {
         // 添加BossBar函数
-        BossBar bar = Bukkit.createBossBar(title, BarColor.PURPLE, BarStyle.SOLID); // 创建BossBar
+        BossBar bar = BossBar.bossBar(title, 1, Color.PURPLE, BossBar.Overlay.NOTCHED_20); // 创建BossBar
         bar.addPlayer(player); // 将BossBar分配给玩家
         bar.setVisible(true); // 设置BossBar为可见
         bars.put(player, bar); // 将映射关系添加进Bars中
@@ -56,11 +62,16 @@ public class BossBarManager {
         bars.clear();
     }
 
-    public static String generateBossBarTitle(Player tracker, Player target) {
+    public static TextComponent generateBossBarTitle(Player tracker, Player target) {
         // 生成BossBar信息函数
-        return ChatColor.YELLOW + "玩家：" + ChatColor.GREEN + target.getName()
-                + ChatColor.YELLOW  + " | 水平方向：" + ChatColor.WHITE + DirectionDistanceCalc.getHorizontalDirection(tracker, target)
-                + ChatColor.YELLOW + " | 垂直方向：" + ChatColor.WHITE + DirectionDistanceCalc.getVerticalDirection(tracker, target)
-                + ChatColor.YELLOW + " | 距离：" + ChatColor.WHITE + DirectionDistanceCalc.getDistance(tracker, target);
+        TextComponent message = Component.text("玩家：", NamedTextColor.YELLOW)
+            .append(Component.text(target.getName()).color(NamedTextColor.GREEN))
+            .append(Component.text(" | 水平方向：").color(NamedTextColor.YELLOW))
+            .append(Component.text(DirectionDistanceCalc.getHorizontalDirection(tracker, target)).color(NamedTextColor.WHITE))
+            .append(Component.text(" | 垂直方向：").color(NamedTextColor.YELLOW))
+            .append(Component.text(DirectionDistanceCalc.getVerticalDirection(tracker, target)).color(NamedTextColor.WHITE))
+            .append(Component.text(" | 距离：").color(NamedTextColor.YELLOW))
+            .append(Component.text(DirectionDistanceCalc.getDistance(tracker, target)).color(NamedTextColor.WHITE));
+        return message;
     }
 }
